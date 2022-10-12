325711954c14d595f62d326413116dd36e8ef9aa
Luís Otávio De Simone <85963351+luisotaviodesimone@users.noreply.github.com>
2022-10-12 00:56:43 -0300

    ✨feat: configure tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import './styles/global.css';
+
 function App() {
   return (
-    <div >
-      <h1>React App</h1>
+    <div>
+      <h1 className="font-bold text-5xl text-violet-500">React App</h1>
+
+      <button className="bg-violet-500 font-medium px-4 py-2 rounded text-white hover:bg-violet-600">Entrar</button>
 
     </div>
   );
