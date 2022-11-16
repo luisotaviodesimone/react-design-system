dec6f708a8795f912f85f4a6f43fa4d27ca69d06
Luís Otávio De Simone <85963351+luisotaviodesimone@users.noreply.github.com>
2022-11-15 21:33:07 -0300

    feat: add libs and configure storybook

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,33 @@ module.exports = {
         sans: 'Inter, sans-serif',
       },
     },
+
+    colors: {
+      black: '#000000',
+      white: '#ffffff',
+
+      gray: {
+        100: '#e1e1e6',
+        200: '#c4c4cc',
+        400: '#7c7c8a',
+        800: '#202024',
+        900: '#121214',
+      },
+
+      cyan: {
+        500: '#81d8f7',
+        300: '#9be1fb',
+      },
+    },
+
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      '2xl': 32,
+    },
   },
   plugins: [],
 };
