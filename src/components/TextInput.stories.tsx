bde1e94c7c8845eab0cc9ca5600739f70a77eb37
Luís Otávio De Simone <85963351+luisotaviodesimone@users.noreply.github.com>
2022-12-16 02:32:25 -0300

    feat: update TextInput component and story

--- src/components/TextInput.stories.tsx
+++ src/components/TextInput.stories.tsx
@@ -1,11 +1,27 @@
 import { Meta, StoryObj } from '@storybook/react';
-import TextInput, { TextInputProps } from './TextInput';
+import { TextInput, TextInputRootProps } from './TextInput';
+import { Envelope } from 'phosphor-react';
 
 export default {
   title: 'Components/TextInput',
-  component: TextInput,
-  args: {},
-  argTypes: {},
-} as Meta<TextInputProps>;
+  component: TextInput.Root,
+  args: {
+    children: (
+      <>
+        <TextInput.Icon>
+          <Envelope />
+        </TextInput.Icon>
+        <TextInput.Input placeholder="Type your email adreses" />
+      </>
+    ),
+  },
+  argTypes: {
+    children: {
+      control: {
+        disable: true,
+      },
+    },
+  },
+} as Meta<TextInputRootProps>;
 
-export const Default: StoryObj<TextInputProps> = {};
+export const Default: StoryObj<TextInputRootProps> = {};
--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, { Children } from 'react';
 import clsx from 'clsx';
 import { Slot } from '@radix-ui/react-slot';
+import {} from 'phosphor-react';
 
-export interface TextInputProps {}
+export interface TextInputInputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  placeholder?: string;
+}
+export interface TextInputRootProps {
+  children: React.ReactNode;
+}
 
-const TextInput = ({}: TextInputProps) => {
+export interface TextInputIconProps {
+  children: React.ReactNode;
+}
+
+const TextInputIcon = ({ children }: { children: React.ReactNode }) => (
+  <Slot className="w6 h6 text-gray-400"></Slot>
+);
+
+const TextInputRoot = ({ children }: TextInputRootProps) => {
   return (
-    <>
-      <input className={clsx('py-4 px-3 rounded bg-gray-800 text-gray-100 text-xs placeholder:text-gray-400')}></input>
-    </>
+    <div className="flex items-center gap-3 rounded bg-gray-800 outline-none focus-within:ring-2 ring-cyan-300">
+      {children}
+    </div>
   );
 };
 
-export default TextInput;
+const TextInputInput = (props: TextInputInputProps) => {
+  return (
+    <input
+      className="bg-transparent flex-1 outline-none py-4 px-3 text-gray-100 text-xs placeholder:text-gray-400"
+      {...props}
+    />
+  );
+};
+
+export const TextInput = {
+  Root: TextInputRoot,
+  Input: TextInputInput,
+  Icon: TextInputIcon,
+};
