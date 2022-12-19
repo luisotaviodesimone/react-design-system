a2e27812357ae2bb968e2586d184b8908b524843
Luís Otávio De Simone <85963351+luisotaviodesimone@users.noreply.github.com>
2022-12-19 00:37:27 -0300

    feat: improve components

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,20 +2,23 @@ import React from 'react';
 import clsx from 'clsx';
 import { Slot } from '@radix-ui/react-slot';
 
-export interface ButtonProps {
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
   asChild?: boolean;
 }
 
-const Button = ({ children, asChild }: ButtonProps) => {
+const Button = ({ children, asChild, className, ...props }: ButtonProps) => {
   const Comp = asChild ? Slot : 'button';
 
   return (
     <>
       <Comp
         className={clsx(
-          'text-gray-900 font-sans text-sm w-full transition-colors focus:ring-2 ring-white bg-cyan-500 px-3 py-4 hover:bg-cyan-300 rounded font-bold'
+          'text-gray-900 font-sans text-sm w-full transition-colors focus:ring-2 ring-white bg-cyan-500 px-4 py-3 hover:bg-cyan-300 rounded font-bold',
+          className
         )}
+        {...props}
       >
         {children}
       </Comp>
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -6,19 +6,24 @@ export interface TextProps {
   size?: 'sm' | 'md' | 'lg';
   children: React.ReactNode;
   asChild?: boolean;
+  className?: string;
 }
 
-const Text = ({ children, size = 'md', asChild }: TextProps) => {
+const Text = ({ children, size = 'md', asChild, className }: TextProps) => {
   const Comp = asChild ? Slot : 'span';
 
   return (
     <>
       <Comp
-        className={clsx('text-gray-100 font-sans', {
-          'text-xs': size === 'sm',
-          'text-sm': size === 'md',
-          'text-md': size === 'lg',
-        })}
+        className={clsx(
+          'text-gray-100 font-sans',
+          {
+            'text-xs': size === 'sm',
+            'text-sm': size === 'md',
+            'text-md': size === 'lg',
+          },
+          className
+        )}
       >
         {children}
       </Comp>
