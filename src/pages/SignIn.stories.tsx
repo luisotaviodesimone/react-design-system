1dc0866b4d8ed43cdc231910128aa83d3e335881
Luís Otávio De Simone <85963351+luisotaviodesimone@users.noreply.github.com>
2022-12-24 18:34:30 -0300

    feat: use testing libraries in sign in page
    38min00

--- src/pages/SignIn.stories.tsx
+++ src/pages/SignIn.stories.tsx
@@ -1,13 +1,29 @@
+import { within, userEvent, waitFor } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
 import { Meta, StoryObj } from '@storybook/react';
 import SignIn from './SignIn';
 
 export default {
   title: 'Pages/Sign In',
   component: SignIn,
-  args: {
-  },
-  argTypes: {
-  },
+  args: {},
+  argTypes: {},
 } as Meta;
 
-export const Default: StoryObj = {};
+export const Default: StoryObj = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    userEvent.type(
+      canvas.getByPlaceholderText('Digite o seu email'),
+      'luis.teste@teste.com'
+    );
+
+    userEvent.type(canvas.getByPlaceholderText('********'), 'password');
+
+    userEvent.click(canvas.getByRole('button'));
+
+    await waitFor(() => {
+      return expect(canvas.getByText('Login Realizado!')).toBeInTheDocument();
+    });
+  },
+};
